f919208eede99915b71545ea52b7bb1f4ca39c1b
Russell Ladd <russell.ladd@grl5.com>
2014-06-25 15:51:00 -0400

    Connected view controller to model

--- NearbyLocator/NLViewController.m
+++ NearbyLocator/NLViewController.m
@@ -8,16 +8,41 @@
 
 #import "NLViewController.h"
 
-@interface NLViewController ()
+#import "NLDirectionManager.h"
+
+@interface NLViewController () <NLDirectionManagerDelegate>
+
+@property (nonatomic, strong, readonly) NLDirectionManager *directionManager;
 
 @end
 
 @implementation NLViewController
 
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    
+    if (self) {
+        
+        _directionManager = [[NLDirectionManager alloc] init];
+        _directionManager.delegate = self;
+    }
+    
+    return self;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self.directionManager startUpdatingDirection];
 }
+
+- (void)directionManager:(NLDirectionManager *)manager didUpdateDirection:(CGFloat)direction
+{
+    self.direction = -direction;
+}
+
 -(void)setDirection:(CGFloat)direction
 {
     _direction = direction;
