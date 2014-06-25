3147332063e50bfe1337dbee99a756bd9e7c82cf
Russell Ladd <russell.ladd@grl5.com>
2014-06-25 15:24:04 -0400

    Initial Commit

---
+++ NearbyLocator/NLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NLAppDelegate.h
+//  NearbyLocator
+//
+//  Created by Russell Ladd on 6/25/14.
+//  Copyright (c) 2014 GRL5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NearbyLocator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NearbyLocator
+//
+//  Created by Russell Ladd on 6/25/14.
+//  Copyright (c) 2014 GRL5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NLAppDelegate class]));
+    }
+}
